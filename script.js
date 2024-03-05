e4129c63d9fb6e4aa834c6af41e40e82642b8bc0
KrilynHje <157597213+KrilynHje@users.noreply.github.com>
2024-03-05 15:47:14 +0100

    Added Feature Sound (working)

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function playMusic(){
- document.getElementById("audio").play();
-}
+document.getElementById('openModal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = "block";
+    document.getElementById('audioPlayer').play();
+   });
+   
+   document.getElementById('closeModal').addEventListener('click', function() {
+    document.getElementById('myModal').style.display = "none";
+   });
+   
+   // Close the modal when the user clicks anywhere outside of it
+   window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal')) {
+       document.getElementById('myModal').style.display = "none";
+    }
+   }
+   
