01825b500601c0011a5119d73b3cfe870215b739
KrilynHje <157597213+KrilynHje@users.noreply.github.com>
2024-03-06 10:16:39 +0100

    Fixed audio issue

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.getElementById('openModal').addEventListener('click', function() {
-    document.getElementById('myModal').style.display = "block";
-    document.getElementById('audioPlayer').play();
+   document.getElementById('myModal').style.display = "block";
+    //document.getElementById('audioPlayer').play();;
+    var audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.volume = 0.15; // Set the volume to 50%
+    audioPlayer.play();
+    
    });
    
    document.getElementById('closeModal').addEventListener('click', function() {
